d3dd0a4bcf5d50de88cd3234aa4100062ec3ee27
Egor <egor.velikiy.02@mail.ru>
2025-12-01 22:40:22 +0300

    feat: user-profile

---
+++ src/mocks/user.ts
@@ -0,0 +1,8 @@
+import type { UserI } from "../types/user";
+
+export const user: UserI = {
+    name: 'Александра Смирнова', 
+    email: 'anymail@mail.ru',
+    avatarUrl: '', 
+    bio: 'Frontend разработчик с 5-летним опытом работы. Люблю создавать красивые и функциональные интерфейсы.'
+}
