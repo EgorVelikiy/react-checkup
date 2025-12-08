7637ab844a7f02abad09faebdca1b3ee5ea34814
Egor <egor.velikiy.02@mail.ru>
2025-12-08 03:09:42 +0300

    feat: editable-users

---
+++ src/mocks/user-list.ts
@@ -0,0 +1,24 @@
+import type { User } from "../types/user";
+
+export const users: User[] = [
+    {
+        id: 1,
+        name: "Александр Иванов"
+    },
+    {
+        id: 2,
+        name: "Мария Петрова",
+    },
+    {
+        id: 3,
+        name: "Дмитрий Смирнов",
+    },
+    {
+        id: 4,
+        name: "Елена Козлова"
+    },
+    {
+        id: 5,
+        name: "Сергей Новиков"
+    }
+]
